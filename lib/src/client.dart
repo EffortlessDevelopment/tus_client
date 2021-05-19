df879522bf1ea0fc8eccc6255e800b61e32b3240
Brandon@Effortless <brandon@effortlessdevelopment.com>
2021-05-19 18:35:15 -0400

    Update client.dart

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -1,4 +1,4 @@
-import 'dart:convert' show base64, utf8;
+import 'dart:convert' show base64, json, utf8;
 import 'dart:math' show min;
 import 'dart:typed_data' show Uint8List;
 import 'exceptions.dart';
@@ -83,7 +83,8 @@ class TusClient {
         "Upload-Length": "$_fileSize",
       });
 
-    final response = await client.post(url, headers: createHeaders);
+    final response = await client
+        .post(url, headers: createHeaders, body: {"Resumable": "true"});
     if (!(response.statusCode >= 200 && response.statusCode < 300) &&
         response.statusCode != 404) {
       throw ProtocolException(
